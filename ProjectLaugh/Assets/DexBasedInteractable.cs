f50444906c7868eeb2e22ef7d2b0d1b66d389ef4
peterino <peterlimail47@gmail.com>
2024-01-28 11:22:44 -0800

    prefab migration

---
+++ ProjectLaugh/Assets/DexBasedInteractable.cs
@@ -0,0 +1,25 @@
+﻿using Dialogue;
+using Dialogue.Gags;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DexBasedInteractable : Interactable
+    {
+        public DialogueSession PassedDialogue;
+        public DialogueSession FailedDialogue;
+        public int requirement = 1;
+
+        public override void OnInteract(GameObject Interactor)
+        {
+            if (GagPlayerAttributes.Get().strength > requirement)
+            { 
+                DialogueSystem.Get().startDialogue(PassedDialogue);
+            }
+            else
+            {
+                DialogueSystem.Get().startDialogue(FailedDialogue);
+            }
+        }
+    }
+}
